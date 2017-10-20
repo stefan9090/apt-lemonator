6f308f17cd0540b1fe8d42cd1296d635c3cc05af
Stefan de Beer <stefan.debeer@student.hu.nl>
2017-10-20 11:03:16 +0000

    Added documentation to simulator_test.py

--- python-tests/simulator_test.py
+++ python-tests/simulator_test.py
@@ -5,6 +5,8 @@ import io
 from contextlib import redirect_stdout
 
 class simulator_test(TestCase):
+
+    #test if heater heats if turned on and if it cools down when turend off.
     def test_heater(self):
         sim = simulator.simulator()
         current_time = 0
@@ -26,8 +28,9 @@ class simulator_test(TestCase):
 
         self.assertGreater(sim.read_real_temp(), original_temp, "Heater needs to heat if turned on")
         self.assertLess(sim.read_real_temp(), value_before_cooling, "Heater needs to cool if turned off")
-        self.assertAlmostEqual(20085, sim.read_real_temp(), 1, "heater test failed")
+        self.assertAlmostEqual(20085, sim.read_real_temp(), 1, "Heater heating and cooling delta are incorrect")
 
+    #Test if sirup pumps and valves work correctly.
     def test_sirup(self):
         sim = simulator.simulator()
         current_time = 0
@@ -45,6 +48,7 @@ class simulator_test(TestCase):
 
         self.assertAlmostEqual(85.8, sim.read_real_mm(), 1, "sirup test failed")
 
+    #Test if water pumps and valves work correctly.
     def test_water(self):
         sim = simulator.simulator()
         current_time = 0
@@ -62,6 +66,7 @@ class simulator_test(TestCase):
 
         self.assertAlmostEqual(85.8, sim.read_real_mm(), 1, "water test failed")
 
+    #Test rampdown of sirup flow when valves stay closed
     def test_valve_sirup(self):
         sim = simulator.simulator()
         current_time = 0
@@ -82,6 +87,7 @@ class simulator_test(TestCase):
 
         self.assertAlmostEqual(73.1, sim.read_real_mm(), 1, "sirup test failed")
 
+    #Test rampdown of water flow when valves stay closed
     def test_valve_water(self):
         sim = simulator.simulator()
         current_time = 0
@@ -102,7 +108,7 @@ class simulator_test(TestCase):
 
         self.assertAlmostEqual(73.1, sim.read_real_mm(), 1, "water test failed")
 
-
+    #Test if the distances sensor returns correct values
     def test_read_mm(self):
         sim = simulator.simulator()
         current_time = 0
@@ -128,6 +134,7 @@ class simulator_test(TestCase):
         self.assertGreater(is_equal, 0, "need to have correct readings")
         self.assertGreater(is_not_equal, 0, "need to have incorrect readings")
 
+    #Test the temperature readings offset of the temperature sensor
     def test_temp_sensor_offset(self):
         sim = simulator.simulator()
         current_time = 0
@@ -141,6 +148,7 @@ class simulator_test(TestCase):
             
         self.assertAlmostEqual(sim.read_real_temp()-700, sim.read_temp(), 0, "read_temp returned wrong values")
 
+    #Test if the pumps don't turn on when there is no cup present and do turn on when there is a cup present
     def test_cup_present(self):
         sim = simulator.simulator()
         sim.cup_present = False
@@ -175,16 +183,19 @@ class simulator_test(TestCase):
         self.assertGreater(sim.read_real_temp(), 20000, "heater did not turn on while cup present")
         self.assertNotEqual(sim.liquid_level, 0, "pumps did not turn on while cup present")
 
+    #Test the get_cup and set_cup function
     def test_set_cup(self):
         sim = simulator.simulator() 
         sim.set_cup(0)
         self.assertEqual(sim.get_cup(), False, "get_cup returned wrong value")
 
+    #Test the set_liquid_level function
     def test_set_liquids_level(self):
         sim = simulator.simulator() 
         sim.set_liquids_level(10)
         self.assertEqual(sim.liquid_level, 10, "setting liquid level failed")
 
+    #Test the limmit on the temperature sensor
     def test_temp_sensor_lim(self):
         sim = simulator.simulator() 
         sim.temp_mc = 80000
